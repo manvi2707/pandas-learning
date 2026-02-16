bc19ab574f215839b95668a1602da6971c8cd66b
Manvi <manvigarg2707@gmail.com>
2026-02-16 11:29:52 +0530

    Read CSV and Json file

--- CsvJson.py
+++ CsvJson.py
@@ -6,3 +6,13 @@ print(df)
 # To print entire datafram and not the truncated version
 # Beware of doing this on large files
 print(df.to_string())
+
+# # Convert entire dataframe to JSON (records format)
+# df.to_json(
+#     "Data/PokemonData.json",
+#     orient="records",
+#     indent=4
+# )
+
+df_json = pd.read_json("Data/PokemonData.json")
+print(df_json)
