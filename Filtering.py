f6921f057fef84c81a1e143bdc348a8e4a98e6be
Manvi <manvigarg2707@gmail.com>
2026-02-17 10:45:36 +0530

    Filtering notes in Pandas

--- Filtering.py
+++ Filtering.py
@@ -7,7 +7,7 @@ df = pd.read_csv("Data/PokemonData.csv")
 tall_pokemon = df[df["Height"] >= 2]
 heavy_pokemon = df[df["Weight"] > 100]
 legendary_pokemon = df[df["Legendary"] == 1] # OR u can write df[df["Legendary" == True]]
-water_pokemon = df[(df["TypeA1"] == "Water") | 
+water_pokemon = df[(df["Type1"] == "Water") | 
                    (df["Type2"] == "Water")]
 
 fire_flying_pokemon = df[((df["Type1"] == "Fire") & (df["Type2"] == "Flying")) | ((df["Type1"] == "Flying") & (df["Type2"] == "Fire"))]
